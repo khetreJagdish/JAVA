4a4790661a223cb24396c314063c30e32f2884a8
Jagdish <khetrejagdish@gmail.com>
2018-06-29 00:09:21 +0530

    Test

---
+++ MultiThreading/src/com/scp/trickyquestions/Test.java
@@ -0,0 +1,10 @@
+package com.scp.trickyquestions;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Math.min(Double.MIN_VALUE, 0.0d));
+		System.out.println(1.0/0.0);
+	}
+}
